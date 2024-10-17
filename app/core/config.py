91c60f8e388576dd44c136954bb35839d8b49baa
Akhil380 <akhilrajr482@gmail.com>
2024-10-18 00:43:18 +0530

    Remove API keys code

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "LMS-MOCK TEST"
     database_url: str
+    sendinblue_api_key: str
 
     class Config:
         env_file = ".env"
